f3d2035d3233cbeb08475b233d3f1a74aeb59ef8
yu-zhang17 <1153580791@qq.com>
2022-07-11 18:33:36 +0800

    lab2 calibration

--- lab2/cost_calibration.py
+++ lab2/cost_calibration.py
@@ -1,3 +1,6 @@
+from math import log
+from sklearn.linear_model import LinearRegression
+
 def estimate_plan(operator, factors, weights):
     cost = 0.0
     for child in operator.children:
@@ -5,7 +8,12 @@ def estimate_plan(operator, factors, weights):
 
     if operator.is_hash_agg():
         # YOUR CODE HERE: hash_agg_cost = input_row_cnt * cpu_fac
-        pass
+        input_row_cnt = 0.0
+        for child in operator.children:
+            input_row_cnt += child.est_row_counts()
+
+        cost += input_row_cnt * factors['cpu']
+        weights['cpu'] += input_row_cnt
 
     elif operator.is_hash_join():
         # hash_join_cost = (build_hashmap_cost + probe_and_pair_cost)
@@ -19,31 +27,71 @@ def estimate_plan(operator, factors, weights):
 
     elif operator.is_sort():
         # YOUR CODE HERE: sort_cost = input_row_cnt * log(input_row_cnt) * cpu_fac
-        pass
+        input_row_cnt = 0.0
+        log_input_row_count = 0.0
+        for child in operator.children:
+            input_row_cnt += child.est_row_counts()
+        if input_row_cnt > 0.0:
+            log_input_row_count = log(input_row_cnt, 2)
+
+        cost += input_row_cnt * log_input_row_count * factors['cpu']
+        weights['cpu'] += input_row_cnt * log_input_row_count
 
     elif operator.is_selection():
         # YOUR CODE HERE: selection_cost = input_row_cnt * cpu_fac
-        pass
+        input_row_cnt = 0.0
+        for child in operator.children:
+            input_row_cnt += child.est_row_counts()
+
+        cost += input_row_cnt * factors['cpu']
+        weights['cpu'] += input_row_cnt
 
     elif operator.is_projection():
         # YOUR CODE HERE: projection_cost = input_row_cnt * cpu_fac
-        pass
+        input_row_cnt = 0.0
+        for child in operator.children:
+            input_row_cnt += child.est_row_counts()
+
+        cost += input_row_cnt * factors['cpu']
+        weights['cpu'] += input_row_cnt
 
     elif operator.is_table_reader():
         # YOUR CODE HERE: table_reader_cost = input_row_cnt * input_row_size * net_fac
-        pass
+        input_row_cnt = 0.0
+        input_row_size = 0.0
+        for child in operator.children:
+            input_row_cnt += child.est_row_counts()
+            input_row_size += child.row_size()
+
+        cost += input_row_cnt * input_row_size * factors['net']
+        weights['net'] += input_row_cnt * input_row_size
 
     elif operator.is_table_scan():
         # YOUR CODE HERE: table_scan_cost = row_cnt * row_size * scan_fac
-        pass
+        row_cnt = operator.est_row_counts()
+        row_size = operator.row_size()
+
+        cost += row_cnt * row_size * factors['scan']
+        weights['scan'] += row_cnt * row_size
 
     elif operator.is_index_reader():
         # YOUR CODE HERE: index_reader_cost = input_row_cnt * input_row_size * net_fac
-        pass
+        input_row_cnt = 0.0
+        input_row_size = 0.0
+        for child in operator.children:
+            input_row_cnt += child.est_row_counts()
+            input_row_size += child.row_size()
+
+        cost += input_row_cnt * input_row_size * factors['net']
+        weights['net'] += input_row_cnt * input_row_size
 
     elif operator.is_index_scan():
         # YOUR CODE HERE: index_scan_cost = row_cnt * row_size * scan_fac
-        pass
+        row_cnt = operator.est_row_counts()
+        row_size = operator.row_size()
+
+        cost += row_cnt * row_size * factors['scan']
+        weights['scan'] += row_cnt * row_size
 
     elif operator.is_index_lookup():
         # index_lookup_cost = net_cost + seek_cost
@@ -91,7 +139,29 @@ def estimate_calibration(train_plans, test_plans):
     # YOUR CODE HERE
     # calibrate your cost model with regression and get the best factors
     # factors * weights ==> act_time
+
+    # print(factors)
+    train_x = []
+    for w in weights:
+        array = [w['cpu'], w['scan'], w['net'], w['seek']]
+        train_x.append(array)
+    model = LinearRegression(fit_intercept=False)
+    result = model.fit(train_x, act_times)
+    # print(res.coef_)
+
+    oldloss = 0.0
+    newloss = 0.0
+
+    for i in range(len(act_times)):
+        oldloss += max(act_times[i] - est_costs_before[i], est_costs_before[i] - act_times[i])
+    newloss = loss(act_times, train_x, result.coef_)
+
+
     new_factors = {
+        "cpu": result.coef_[0],
+        "scan": result.coef_[1],
+        "net": result.coef_[2],
+        "seek": result.coef_[3],
     }
     print("--->>> regression cost factors: ", new_factors)
 
@@ -103,3 +173,13 @@ def estimate_calibration(train_plans, test_plans):
         est_costs.append(cost)
 
     return est_costs
+
+def loss(act_times, weights, factors):
+    myloss = 0.0
+    assert(len(act_times) == len(weights))
+    for i in range(len(act_times)):
+        est_costs = 0.0
+        for j in range(4):
+            est_costs += weights[i][j] * factors[j]
+        myloss += max(act_times[i]-est_costs, est_costs-act_times[i])
+    return myloss
--- lab2/evaluation.py
+++ lab2/evaluation.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
     test_json_file = 'data/test_plans.json'
     train_plans, test_plans = [], []
 
-    with open(train_json_file, 'r') as f:
+    with open(train_json_file, 'r', encoding='utf-8') as f:
         train_cases = json.load(f)
     for case in train_cases:
         train_plans.append(Plan.parse_plan(case['query'], case['plan']))
 
-    with open(test_json_file, 'r') as f:
+    with open(test_json_file, 'r', encoding='utf-8') as f:
         test_cases = json.load(f)
     for case in test_cases:
         test_plans.append(Plan.parse_plan(case['query'], case['plan']))
@@ -27,17 +27,17 @@ if __name__ == '__main__':
         act_times.append(p.exec_time_in_ms())
         tidb_costs.append(p.tidb_est_cost())
 
-    _, _, est_learning_costs, act_learning_times = estimate_learning(train_plans, test_plans)
+    # _, _, est_learning_costs, act_learning_times = estimate_learning(train_plans, test_plans)
     est_calibration_costs = estimate_calibration(train_plans, test_plans)
 
     draw_act_est_figure("tidb", tidb_costs, act_times, True)
-    draw_act_est_figure("learning", est_learning_costs, act_learning_times, True)
+    # draw_act_est_figure("learning", est_learning_costs, act_learning_times, True)
     draw_act_est_figure("calibration", est_calibration_costs, act_times, True)
 
     with open('./eval/results.json', 'w') as outfile:
         json.dump({
             "act": act_times,
             "tidb": tidb_costs,
-            "learning": est_learning_costs,
+            # "learning": est_learning_costs,
             "calibration": est_calibration_costs,
         }, outfile)
